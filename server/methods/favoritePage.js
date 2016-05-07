2c3679a6e7ea9a10e298edd3b438668ebd214773
Guidouil <guidouil@gmail.com>
2016-05-07 22:50:26 +0200

    secured methods

--- server/methods/favoritePage.js
+++ server/methods/favoritePage.js
@@ -1,15 +1,17 @@
 Meteor.methods({
   favoritePage: function (pageId) {
-    var page = Pages.findOne({_id: pageId});
-    if (page.isPublic || isPageOwner(pageId, this.userId) || isPageUser(pageId, this.userId)) {
-      if (_.contains(page.favorites, this.userId)) {
-        Pages.update({ _id: pageId }, { $pull: {
-          favorites: this.userId
-        }});
-      } else {
-        Pages.update({ _id: pageId }, { $push: {
-          favorites: this.userId
-        }});
+    if (this.userId) {
+      var page = Pages.findOne({_id: pageId});
+      if (page.isPublic || isPageOwner(pageId, this.userId) || isPageUser(pageId, this.userId)) {
+        if (_.contains(page.favorites, this.userId)) {
+          Pages.update({ _id: pageId }, { $pull: {
+            favorites: this.userId
+          }});
+        } else {
+          Pages.update({ _id: pageId }, { $push: {
+            favorites: this.userId
+          }});
+        }
       }
     }
   }
--- server/methods/getPageOwnersAndUsers.js
+++ server/methods/getPageOwnersAndUsers.js
@@ -1,86 +1,96 @@
 Meteor.methods({
   getPageOwnersAndUsers: function (pageId) {
-    check(pageId, String);
-    if (isPageOwner(pageId, this.userId)) {
-      var page = Pages.findOne({_id: pageId});
-      var owners = [];
-      _.each(page.owners, function (ownerId) {
-        var owner = Meteor.users.findOne({_id: ownerId});
-        owners.push({
-          userId: ownerId,
-          email: contactEmail(owner)
-        });
-      });
-      var users = [];
-      if (page.users) {
-        _.each(page.users, function (userId) {
-          var user = Meteor.users.findOne({_id: userId});
-          users.push({
-            userId: userId,
-            email: contactEmail(user)
+    if (this.userId) {
+      check(pageId, String);
+      if (isPageOwner(pageId, this.userId)) {
+        var page = Pages.findOne({_id: pageId});
+        var owners = [];
+        _.each(page.owners, function (ownerId) {
+          var owner = Meteor.users.findOne({_id: ownerId});
+          owners.push({
+            userId: ownerId,
+            email: contactEmail(owner)
           });
         });
+        var users = [];
+        if (page.users) {
+          _.each(page.users, function (userId) {
+            var user = Meteor.users.findOne({_id: userId});
+            users.push({
+              userId: userId,
+              email: contactEmail(user)
+            });
+          });
+        }
+        return {'owners': owners, 'users': users};
       }
-      return {'owners': owners, 'users': users};
     }
     return false;
   },
   giveOrwnership: function (email, pageId) {
-    check(email, String);
-    check(pageId, String);
-    if (isPageOwner(pageId, this.userId)) {
-      var owner = Meteor.users.findOne({$or:[
-        { 'emails.address': email },
-        { 'user.services.facebook.email': email },
-        { 'user.services.google.email': email }
-      ]});
-      if (owner) {
-        Pages.update({_id: pageId}, {$addToSet: {
-          owners: owner._id
-        }});
-        return true;
+    if (this.userId) {
+      check(email, String);
+      check(pageId, String);
+      if (isPageOwner(pageId, this.userId)) {
+        var owner = Meteor.users.findOne({$or:[
+          { 'emails.address': email },
+          { 'user.services.facebook.email': email },
+          { 'user.services.google.email': email }
+        ]});
+        if (owner) {
+          Pages.update({_id: pageId}, {$addToSet: {
+            owners: owner._id
+          }});
+          return true;
+        }
       }
       return false;
     }
   },
   removeOwnership: function (ownerId, pageId) {
-    check(ownerId, String);
-    check(pageId, String);
-    if (isPageOwner(pageId, this.userId)) {
-      Pages.update({_id: pageId}, {$pull: {
-        owners: ownerId
-      }});
-      return true;
+    if (this.userId) {
+      check(ownerId, String);
+      check(pageId, String);
+      if (isPageOwner(pageId, this.userId)) {
+        Pages.update({_id: pageId}, {$pull: {
+          owners: ownerId
+        }});
+        return true;
+      }
     }
     return false;
   },
   addUser: function (email, pageId) {
-    check(email, String);
-    check(pageId, String);
-    if (isPageOwner(pageId, this.userId)) {
-      var user = Meteor.users.findOne({$or:[
-        { 'emails.address': email },
-        { 'user.services.facebook.email': email },
-        { 'user.services.google.email': email }
-      ]});
-      if (user) {
-        Pages.update({_id: pageId}, {$addToSet: {
-          users: user._id
-        }});
-        return true;
+    if (this.userId) {
+      check(email, String);
+      check(pageId, String);
+      if (isPageOwner(pageId, this.userId)) {
+        var user = Meteor.users.findOne({$or:[
+          { 'emails.address': email },
+          { 'user.services.facebook.email': email },
+          { 'user.services.google.email': email }
+        ]});
+        if (user) {
+          Pages.update({_id: pageId}, {$addToSet: {
+            users: user._id
+          }});
+          return true;
+        }
       }
       return false;
     }
   },
   removeUser: function (userId, pageId) {
-    check(userId, String);
-    check(pageId, String);
-    if (isPageOwner(pageId, this.userId)) {
-      Pages.update({_id: pageId}, {$pull: {
-        users: userId
-      }});
-      return true;
+    if (this.userId) {
+      check(userId, String);
+      check(pageId, String);
+      if (isPageOwner(pageId, this.userId)) {
+        Pages.update({_id: pageId}, {$pull: {
+          users: userId
+        }});
+        return true;
+      }
+      return false;
     }
-    return false;
   }
 });
